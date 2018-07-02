66b81a0fc92ca44897cb7ce4bf45c837e5d88ef9
patdonato <prbdonato@gmail.com>
2018-07-02 19:29:53 +0800

    modified some codes for the means

--- run_analysis.R
+++ run_analysis.R
@@ -12,18 +12,18 @@ unzip(zipfile = "HAR.zip")
 
 
 ## Load and clean labels and features into R
-setwd("C:/Users/HP/Desktop/JHU Getting and Cleaning Data/Week 4/Course Project/UCI HAR Dataset")
+setwd("C:/Users/HP/Desktop/JHU Getting and Cleaning Data/Week 4/UCI HAR Dataset")
 
 activity_data <- read.table("activity_labels.txt", sep = '',  col.names = c("activity_ID","activity_name"),stringsAsFactors= FALSE)
 activity_data$activity_name <- tolower(activity_data$activity_name)
 activity_data$activity_name <- str_replace_all(activity_data$activity_name, '_', ' ')
 
-features_data <- read.table("features.txt",sep = '', stringsAsFactors = FALSE)
+feature_data <- read.table("features.txt",sep = '', stringsAsFactors = FALSE)
 
 
 ## Extract mean and sd only
-with_mean <- str_which(features_data$V2, '.*mean[^Freq]')
-with_sd <- str_which(features_data$V2, '.*std.*')
+with_mean <- str_which(feature_data$V2, '.*mean[^Freq]')
+with_sd <- str_which(feature_data$V2, '.*std.*')
 
 
 ## Make column names to be used later
@@ -37,7 +37,7 @@ feature_names <- filter(feature_data, V1 %in% c(with_mean, with_sd))
 
 
 ## Load and clean test into R
-setwd("C:/Users/HP/Desktop/JHU Getting and Cleaning Data/Week 4/Course Project/UCI HAR Dataset/test")
+setwd("C:/Users/HP/Desktop/JHU Getting and Cleaning Data/Week 4/UCI HAR Dataset/test")
 
 test_data <- read.table("subject_test.txt", sep = "", col.names = c("subject_ID"))
 test_y <- read.table("y_test.txt", sep = "", col.names = c("activity_ID"))
@@ -48,7 +48,7 @@ test_final <- cbind(test_data, test_y, test_x[,feature_names$V2])
  
 
 ## Load and clean train into R
-setwd("C:/Users/HP/Desktop/JHU Getting and Cleaning Data/Week 4/Course Project/UCI HAR Dataset/train")
+setwd("C:/Users/HP/Desktop/JHU Getting and Cleaning Data/Week 4/UCI HAR Dataset/train")
 
 train_data <- read.table("subject_train.txt", sep = "", col.names = c("subject_ID"))
 train_y <- read.table("y_train.txt", sep = "", col.names = c("activity_ID"))
@@ -65,7 +65,8 @@ final_data <- merge(full_data, activity_data, by = "activity_ID") # assures that
 setwd("C:/Users/HP/Desktop/JHU Getting and Cleaning Data/Week 4/Course Project")
 
 final_data %>%
-    melt(id = c("activity_name", "subject_ID")) %>%
-    group_by(activity_name, subject_ID) %>%
+    select(-1) %>%
+    melt(id = c("subject_ID","activity_name")) %>%
+    group_by(subject_ID, activity_name, variable) %>%
     summarise(avg = mean(value, na.rm = TRUE)) %>%
     write.table(file = "tidydata.txt", row.names = FALSE)
